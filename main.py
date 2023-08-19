986c2438d043c98a0d3dddb8cf7dfa25724d6268
Nowidas <lukaszkrupczak@gmail.com>
2023-08-19 22:42:04 +0200

    read default schedule

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+def read_schedule(filename: str) -> dict:
+    schedule = {}
+    with open(filename, "r") as file:
+        for line in file:
+            day, hours = line.strip().split("\t")
+            schedule[int(day)] = int(hours)
+    return schedule
+
+
 def main() -> None:
-    pass
+    filename = "harmonogram.csv"  # Wprowadź nazwę pliku z harmonogramem
+    schedule = read_schedule(filename)
 
 
 if __name__ == "__main__":
